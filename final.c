4271789b96e44ff5ce45899e25f13cbe2a087912
LeVeloute <paul.fontaine@viacesi.fr>
2016-12-12 07:32:33 -0800

    Version 3
    + Gère centrage des objets

--- final.c
+++ final.c
@@ -10,19 +10,21 @@
 
 
 int tab_editeur(char **A, int Xinit, int Yinit, char *file){
+
 	struct winsize w; // Création d'une structure contenant la taille de la console
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); // Ecriture de la taille de la console dans la structure "w"
-	int X,Y, i=0, compteur=0;
+	int X,Y, i=0, compteur=0, stop=0;
 	char nb_magique[1024]="", ligne[1024]="", cara, *token, longueur[1024]="", largeur[1024]="";
-	X = Xinit;
-	Y = Yinit;
 	FILE* fichier = NULL;
 	fichier = fopen(file, "r");
+	X = Xinit;
+	Y = Yinit;
 
     while(fgets(ligne,1024,fichier)!= NULL){
 
     	if (strchr(ligne, '#') == NULL){
     		compteur++;
+    		//printf("%d\n",compteur);
     		switch(compteur){
     		case 1:
     			strcpy(nb_magique,ligne);
@@ -33,26 +35,39 @@ int tab_editeur(char **A, int Xinit, int Yinit, char *file){
 	    		int longu = atoi(longueur);
 	    		token = strtok(NULL," ");
     			strcpy(largeur,token);
+    			int larg = atoi(largeur);
+
+    			if(Xinit == 666){
+    				Xinit = (w.ws_col-longu)/2;
+    				X = Xinit;
+    				//printf("%d", X);
+
+    			}
+    			if(Yinit == 666 && stop==0){
+    				Y = (w.ws_row-larg)/2;
+    				//printf("%d", Y);
+    				stop =1;
+    			}
     			break;
     		default:
+    			// printf("hey");
+    			// printf(" x: %d\n", X);
     			if(Y<=0){
     				for(Y; Y<=0; Y++){
     					fgets(ligne,1024,fichier);
     				}
     			}
     			token = strtok(ligne," ");
-    			printf("%s",token);
     			while(token != NULL){
     				if(X<=w.ws_col || Y<=w.ws_row){
     					if(Y>=w.ws_row) return 0;
 	    				if(strncmp(token, "0",1) == 0){
-	    					printf("pass 0");
 	    					A[Y][X]=' ';
 	    					X++;
 	    					i++;
 	    				}
 	    				if(strncmp(token, "1",1) == 0){
-	    					A[Y][X]='X';
+	    					A[Y][X]='█';
 	    					X++;
 	    					i++;
 	    				}
@@ -110,7 +125,10 @@ int main(int argc, char *argv[]){
 	system("clear");
 	// tab_afficheur(A);
 	tab_editeur(A,10,10,"screen1.pbm");
-	tab_editeur(A,100,-5,"screen1.pbm");
+	tab_editeur(A,666,666,"screen1.pbm");
+		tab_editeur(A,14,666,"screen1.pbm");
+			tab_editeur(A,6,666,"screen1.pbm");
+				tab_editeur(A,66,64,"screen1.pbm");
 	tab_afficheur(A);
 
 
