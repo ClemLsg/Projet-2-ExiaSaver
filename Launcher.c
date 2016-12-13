8eaa39f6657fc931ecae049eb8865311d409194b
enzo-billis <enzo.billis@viacesi.fr>
2016-12-13 23:45:35 +0100

    ScreenSaver3 integré + Commentaires complets

--- Launcher.c
+++ Launcher.c
@@ -1,23 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <termios.h>
-#include <time.h>
+#include <time.h>                                                       //Bibliothèque permettant de gerer le temps
+#include <string.h>                                                     //Bibliothèque permettant de gerer les chaines de charactère
+#include <unistd.h>                                                     //Bibliothèque permettant de gerer les symbols standard
+#include <sys/types.h>                                                  //Bibliothèque permettant de gerer les définitions de temps pour UNIX
+#include <termios.h>                                                    //Bibliothèque permettant de gerer les définitions des valeurs du termios
 
-#define TAILLE_MAX 40
-#define TRUE 1
-#define FALSE 0
+#define TAILLE_MAX 40                                                   //Définit TAILLE_MAX à 40
+#define TRUE 1                                                          //Définit TRUE comme 1
+#define FALSE 0                                                         //Définit FALSE comme 0
 
-FILE* fichierstats = NULL;                                              /*Déclare la variable fichierstats comme variable fichier*/
-char chainehisto[40960][40];
+FILE* fichierstats = NULL;                                              /*Déclare la variable fichierstats comme variable globale fichier*/
+char chainehisto[40960][40];                                            //Donne a chainehisto des valeurs globales
 
-int transformation(int n)
-{
-    switch(n){
-        case 49:
+int transformation(int n)                                               //Fonction permettant la transformation du code uni en valeur
+{                                                                       //La récupération du char en int donne des code uni
+    switch(n){                                                          //Ici on les transforme en entier, ce qui...
+        case 49:                                                        //Leur redonne la valeur d'origine
             n=1;
             break;
         case 50:
@@ -30,45 +29,44 @@ int transformation(int n)
     return n;
 }
 
-void tri_bulle(int n)
+void tri_bulle(int n)                                                   //Fonction de tri à bulle ( Se ref. wikipedia tri a bulle)
 {
-    int j = 0;
-    char tmp[2][40];
-    int pastrie = 1;
-    int p;
-    int a;
-    int b;
-    char temp;
-    int typea=0;
-    int typeb=0;
-    int typec=0;
-    while(pastrie)
+    int j = 0;                                                          //Déclaration d'un compteur entier
+    char tmp[2][40];                                                    //Déclaration d'un tableau de char en 2D de 2x40
+    int pastrie = 1;                                                    //Déclaration d'un int pastrie, si 1 alors le tri n'est pas finit                                                              //
+    int a;                                                              //Déclaration d'un entier prenant le code UNI d'un char
+    int b;                                                              //Déclaration d'un entier prenant le code UNI d'un char
+    char temp;                                                          //Déclaration d'une variable char tempon, contient la chaine en cours de transfert dans le tri à bulle
+    int typea=0;                                                        //Déclaration de variable contenant le nombre de fois qu'est utilisé l'ES pour les statiques
+    int typeb=0;                                                        //Déclaration de variable contenant le nombre de fois qu'est utilisé l'ES pour les dynamiques
+    int typec=0;                                                        //Déclaration de variable contenant le nombre de fois qu'est utilisé l'ES pour les intéractifs
+    while(pastrie)                                                      //Boucle qui tourne tant que le tri n'est pas finis
     {
-        pastrie=FALSE;
-        for(j=0;j<n;j++)
+        pastrie=FALSE;                                                  //Définit de base le tri comme non finit (juste pour être sur)
+        for(j=0;j<n;j++)                                                //Boucle qui tourne jusqua que toute les lignes du char chainehisto soient traitées
         {
-            a=(int) chainehisto[j][20];
-            b=(int) chainehisto[j+1][20];
-            a=transformation(a);
+            a=(int) chainehisto[j][20];                                 //Prend la valeur UNI du char étant à la ligne J au rand 20, emplacement exacte du numéro de mode dans le fichier histo
+            b=(int) chainehisto[j+1][20];                               //Prend la valeur UNI du char étant à la ligne J+1 (la ligne suivante la varible a ) au rand 20
+            a=transformation(a);                                        //Ici on appelle la fonction transformation pour transformer le code UNI en int
             b=transformation(b);
 
-            if(a>b){
-                strcpy(tmp[0],chainehisto[j+1]);
-                strcpy(chainehisto[j+1],chainehisto[j]);
-                strcpy(chainehisto[j],tmp[0]);
-                pastrie=TRUE;
+            if(a>b){                                                    //Si, grace a la transformation on comprend que a est supérieur à b, alos...
+                strcpy(tmp[0],chainehisto[j+1]);                        //On effectue le tri à bulle c-a-d on prend le trop grand, on le met dans une variable temporaire
+                strcpy(chainehisto[j+1],chainehisto[j]);                //Ensuite on met le plus petit à la place du plus grand et on met le plus grand a la place du plus petit.
+                strcpy(chainehisto[j],tmp[0]);                          //Grace a ça on ecrase aucune donnée.
+                pastrie=TRUE;                                           //On signale le tri comme terminé mais ne sera effectife que quand le boucle for sera finie
             }
             
         }
     }
-    n=n+1;
-    for (j=0; j<n; j++){
-        printf("%s",chainehisto[j]);
-        a=(int) chainehisto[j][20];
-        a=transformation(a);
-        switch(a){
-                case 1:
-                    typea++;
+    n=n+1;                                                              //On incrémente n, car le 0 étant compté precedement pour le déplacement dans les tableaux, pour avoir un affichage plus propre pour l'utilisateur
+    for (j=0; j<n; j++){                                                //Ici on affiche la chainehisto trié ligne par ligne
+        printf("%s",chainehisto[j]);                                    //Tant que toutes les lignes ne sont pas passées
+        a=(int) chainehisto[j][20];                                     //On récupère de nouveau la valeur UNI
+        a=transformation(a);                                            //On la traite
+        switch(a){                                                      //Et en fonction du résultat on incrément des variables pour
+                case 1:                                                 //donner à l'utilisateur les statistiques et le
+                    typea++;                                            // nombre de fois que chaque ES à été lancé.
                     break;
                 case 2:
                     typeb++;
@@ -83,50 +81,55 @@ void tri_bulle(int n)
 }
     
 
-void statistiques()
+void statistiques()                                                 //Fonction permettant l'affichage des statistiques
 {
-    char chaine[TAILLE_MAX] = "r";
-    int modetri;
-    int i;
-    int j;
-    int *t;
-    int n;
-    fichierstats = fopen("historique", "r" );
+    char chaine[TAILLE_MAX] = "r";                                  //Variable chaine définie avec une taille par DEFINE qui contiendra la chaine en cours d'utilisation
+    int modetri;                                                    //Varibale qui contient le mode de triage choisit par l'utilisateur
+    int i=0;                                                          //Variable qui sert de compteur pour le nombre de ligne dans le fichier historique
+    int j;                                                          //Compteur
+    int n;                                                          //Compteur
+    fichierstats = fopen("historique", "r" );                       //Ouvre le fichier d'historique avec les droits de lecture
     if (fichierstats==NULL){ 
-                printf("Erreur d'ouverture fichier"); 
-            }
-    i=0;
-    while (strcmp(chaine," ")!=0)
+                printf("Erreur d'ouverture fichier");               //Affiche une erreur si le fichier n'est pas ouvrable
+            }                                                        
+    while (strcmp(chaine," ")!=0)                                   //Tant que la variable chaine ne contient pas un blanc....
     {
-        fgets(chaine, TAILLE_MAX, fichierstats);
-        if (strcmp(chaine," ")!=0)
+        fgets(chaine, TAILLE_MAX, fichierstats);                    //On récupère ligne par ligne le fichier historique et....
+        if (strcmp(chaine," ")!=0)                                  //Si ce qu'on recupère n'est toujours pas un blanc
         {
-            strcat(chainehisto[i], chaine);
+            strcat(chainehisto[i], chaine);                         //On colle ce qu'on vient de récuperer dans une variable char tampon qui contiendra alors l'ensemble du fichier historique sous forme de varibale, le tout dans le bon format (saut de lignes inclus)
         }
-        i++;
+        i++;                                                        //On incrément i pour ajouter chaine dans une autre ligne du tableau chainehisto ( pour garder un format lisible)
     }
-    fclose(fichierstats);
-    i=i-1;
-    while (modetri!=1 && modetri!=2 && modetri!=3)
+    fclose(fichierstats);                                           //On ferme le fichier historique
+    i=i-1;                                                          //On decrémente i une fois car la boucle while nous compte une ligne de trop
+    while (modetri!=1 && modetri!=2 && modetri!=3)                  //Ici tant que l'utilisateur rentre pas un mode de tri valide on continue de demander
     {
-        printf("Pour trier par date tapez 1, par type tapez 2 et par paramètres tapez 3 :\n");
-        scanf("%d",&modetri);
+        printf("Pour trier par date tapez 1, par type tapez 2 et par dates inversées tapez 3 :\n");
+        scanf("%d",&modetri);                                       //On place dans la varible modetri le choix de l'utilisateur
     }
     printf("Voici votre historique ");
-    switch(modetri){
-        case 1:
+    switch(modetri){                                                //Ici en fonction du choix de l'utilisateur, on lance nos différents tri
+        case 1:                                                     //Si modetri=1 ....
             printf("trié par date:\n\n");
-            for (j=0;j<i;j++)
+            for (j=0;j<i;j++){                                      //Ici on affiche ligne par ligne avec le compteur j qu'on incrémente...
+                printf("Utilisation N°%d:  ",j+1);                  //les différentes lignes du tableau chainehisto
                 printf("%s\n",chainehisto[j]);
-            printf("Vous avez lancé %d fois ExiaSaver !\n",i);
-            break;
-        case 2:
+            }
+            printf("Vous avez lancé %d fois ExiaSaver !\n",i);      //On indique à l'utilisateur combien de fois il a utilisé l'ES
+            break;                                                  //Met fin au cas 1
+        case 2:                                                     //Si modetri=2 ....
             printf("trié par type:\n\n");
-            tri_bulle(i);
+            tri_bulle(i);                                           //On appelle la fonction tri à bulle avec l'argument i, qui est le nombre de lignes
             
             break;
-        case 3:
-            printf("trié par paramètres:\n\n");
+        case 3:                                                     //Si modetri=3 ....
+            printf("trié par dates inversées:\n\n");
+             for (j=i;j>=0;j--){                                    //On inverse la boucle du mode 1
+                printf("Utilisation N°%d:  ",j+1);                  //ce qui a pour effet d'afficher dans l'autre sens
+                printf("%s\n",chainehisto[j]);                      //les entrées de chainehisto
+            }
+            printf("Vous avez lancé %d fois ExiaSaver !\n",i);      //On indique à l'utilisateur combien de fois il a lancé l'ES
             break;
     }
 
@@ -186,52 +189,45 @@ char getch()                                                            /*Foncti
 
 int main (int agrc, char * argv[])
 {
-    printf("Test\n");
-    char data[16];
-    char datata[16];
-    char *fichier;
-    int f;
-    int random;
-    int i;
+    char data[16];                                              //Variable contenant le pramètre de lancement du ScreenSaver
+    char *fichier;                                              //Variable contenant le nom du fichier à executer pour le ScreenSaver
+    int f;                                                      //Variable pour le Fork. Contient 0 si fils et le PID du processus si père
+    int random;                                                 //Variable contenant une valeur aléatoire en 1 et 3 pour le lancement du SS
 
-    system("clear");
-    if (agrc>1)
+    system("clear");                                            //Commande vidant la console, affiche plus propre
+    if (agrc>1)                                                 //Condition valide si agrc (nombre de paramètres lancés avec ES) est supérieur à 1
     {
-        if (strcmp(argv[1],"-stats"))
+        if (strcmp(argv[1],"-stats"))                           //Condition valide si argv (valeur des pramètres lancés avec ES) vaut -stats
         {
-            printf("Erreur de paramètres\n");
+            printf("Erreur de paramètres\n");                   
         }
         else
         {
             printf("Lancement des stats...\n");
-            /*while (getch()!='q')
-            {
-
-            }*/
-            statistiques();
+            statistiques();                                     //Lancement de la fonction statistiques
         }
     }  
-    else
+    else                                                        //Arrive ici si le ES n'est pas lancé avec des arguments
     {
         printf("Lancement du screensaver...\n");
-        srand(time(NULL));                                      /*Graine permettant d'éviter la repetition*/
-        random = rand();
-        random = (random%3);
-        if (random==0)
+        srand(time(NULL));                                      //Graine permettant d'éviter la repetition lors du randomize
+        random = rand();                                        //Place dans random une valeur aléatoire sans limites à partir de la seed
+        random = (random%3);                                    //%3 réduit le résultat à une plage en 0 et 2 soit 3 valeurs
+        if (random==0)                                          //Si le nombre genere est 0 alors...
         {
-            f=fork();
-            if (f==0)
+            f=fork();                                           //Lance un processus fils 
+            if (f==0)                                           //Uniquement le processus fils avec un PID de 0 peut entrer dans la boucle
             {
-                strcpy(data,"grenouille.pbm");
-                entreehistorique(random,data);
-                fichier="afficheur";
+                strcpy(data,"grenouille.pbm");                  //On met la chaîne de caractère grenouille.pbm dans la variable data
+                entreehistorique(random,data);                  //On entre dans la fonction entreehistorique avec les arguments random et data
+                fichier="afficheur";                            //On donne à fichier la chaine de caratcère afficheur qui correspond au fichier à ouvrir
                 printf("Demarrage du statique\n");
-                execl(fichier,fichier,NULL);
-                exit(1);
+                execl(fichier,fichier,"666","666",data,NULL);       //On execute le fichier afficheur avec l'argument 666 qui position au centre de la console l'image
+                exit(1);                                        //On tue le processus fils
             }
-            else
+            else                                                
             {
-                wait();
+                wait();                                         //Sinon le père attend l'arrivée ou la mort du fils
             }
         }
         if (random==1)
@@ -239,8 +235,8 @@ int main (int agrc, char * argv[])
             f=fork();
             if (f==0)
             {
-                strcpy(data,"10x10");
-                entreehistorique(random,data);
+                strcpy(data,"10x10");                           //On rentre dans data des valeurs
+                entreehistorique(random,data);                  
                 printf("Demarrage du dynamique\n");
                 exit(1);
             }
@@ -256,9 +252,10 @@ int main (int agrc, char * argv[])
             {
                 strcpy(data,"20x20");
                 entreehistorique(random,data);
-                fichier="avion";
+                fichier="screensaver";
                 printf("Demarrage de l'interactif\n");
-                execl(fichier,fichier,NULL);
+                execl(fichier,fichier,"20","20","2",NULL);
+                perror("Erreur: ");
                 exit(1);
             }
             else
