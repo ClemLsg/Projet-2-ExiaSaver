ef074571c9dd944b79dd2464e882b4aea1d6b99d
Darkdady <clement.lesage@viacesi.fr>
2016-12-14 10:24:19 +0100

    Mise a jour du tab.h

--- tab.h
+++ tab.h
@@ -1,4 +1,4 @@
 
-int tab_editeur(char **A, int Xinit, int Yinit, char *file);
+int tab_editeur(char **A, int Xinit, int Yinit, char *file, int tail);
 void tab_afficheur(char **A);
 void tab_clear(char **A);
